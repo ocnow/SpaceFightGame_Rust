f081000e45387063bd1505eb00f1171a88b68fe8
ocnow <ockulkarni97@gmail.com>
2024-05-22 22:24:49 +0530

    basic_game done

---
+++ src/engine/mod.rs
@@ -0,0 +1,3 @@
+pub fn trySomething(){
+    println!("thisn is someting");
+}
---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use bevy::prelude::*;
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, (hello_world, (update_people, greet_people).chain()));
+    }
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaine Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Elaine Proctor" {
+            name.0 = "Elaine Hume".to_string();
+            break;
+        }
+    }
+}
+fn hello_world() {
+    println!("hello world")
+}
+
+fn main() {
+    App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
+}
