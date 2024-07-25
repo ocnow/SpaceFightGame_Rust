8fcb9cd5c33ed93f8d33802e5237f749d9b9ac76
ocnow <ockulkarni97@gmail.com>
2024-07-26 03:42:26 +0530

    bullet ready and hardcode window size removed

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 use bevy::{
-    core_pipeline::tonemapping::Tonemapping, prelude::*, sprite::MaterialMesh2dBundle,
-    window::WindowResized,
+    core_pipeline::tonemapping::Tonemapping,  prelude::*, sprite::MaterialMesh2dBundle, window::{PrimaryWindow, WindowResized}
 };
+
+#[derive(Component)]
+struct SpacePoint;
+
+#[derive(Component)]
+struct Jet;
+
+#[derive(Component)]
+struct Bullet;
+
+
+#[derive(Resource)]
+struct SpacePointTimer(Timer);
 
 fn setup_system(
     mut commands: Commands,
@@ -18,36 +30,47 @@ fn setup_system(
         ..default()
     },));
 
-    commands.spawn(SpriteBundle {
+    commands.spawn((Jet,
+        SpriteBundle {
         texture: asset_server.load("jet.png"),
         sprite: Sprite {
-            color: Color::rgb(5.0, 5.0, 5.0), // 4. Put something bright in a dark environment to see the effect
+            color: Color::srgb(5.0, 5.0, 5.0), // 4. Put something bright in a dark environment to see the effect
             custom_size: Some(Vec2::splat(160.0)),
             ..default()
         },
         ..default()
-    });
+    }));
 
-    for i in -80..80 {
-        for j in 0..80 {
-            commands.spawn(MaterialMesh2dBundle {
-                mesh: meshes.add(Capsule2d::new(5.0, 5.0)).into(),
-                material: materials.add(Color::rgb(7.5, 0.0, 7.5)),
-                transform: Transform::from_translation(Vec3::new((j * 50) as f32, i as f32 * 50., 0.)),
-                ..default()
-            });
-            commands.spawn(MaterialMesh2dBundle {
-                mesh: meshes.add(Capsule2d::new(5.0, 5.0)).into(),
-                material: materials.add(Color::rgb(7.5, 0.0, 7.5)),
-                transform: Transform::from_translation(Vec3::new(
-                    (-1 * (j * 50)) as f32,
-                    i as f32 * 50.0,
-                    0.,
-                )),
-                ..default()
-            });
-        }
-    }
+    // for i in -50..50 {
+    //     for j in 0..50 {
+    //         commands.spawn((
+    //             SpacePoint,
+    //             MaterialMesh2dBundle {
+    //                 mesh: meshes.add(Circle::default()).into(),
+    //                 material: materials.add(Color::srgb(7.5, 0.0, 7.5)),
+    //                 transform: Transform::from_translation(Vec3::new(
+    //                     (j * 20) as f32,
+    //                     i as f32 * 20.,
+    //                     0.,
+    //                 )),
+    //                 ..default()
+    //             },
+    //         ));
+    //         commands.spawn((
+    //             SpacePoint,
+    //             MaterialMesh2dBundle {
+    //                 mesh: meshes.add(Circle::default()).into(),
+    //                 material: materials.add(Color::rgb(7.5, 0.0, 7.5)),
+    //                 transform: Transform::from_translation(Vec3::new(
+    //                     (-1 * (j * 20)) as f32,
+    //                     i as f32 * 20.,
+    //                     0.,
+    //                 )),
+    //                 ..default()
+    //             },
+    //         ));
+    //     }
+    // }
 }
 
 fn on_resize_system(mut resize_reader: EventReader<WindowResized>) {
@@ -57,15 +80,63 @@ fn on_resize_system(mut resize_reader: EventReader<WindowResized>) {
     }
 }
 
-fn update_system(mut query : Query<>) {
-    println!("hello this is update system")
+fn update_background(
+    mut query: Query<&mut Transform, With<SpacePoint>>,
+    time: Res<Time>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    mut timer: ResMut<SpacePointTimer>,
+) {
+    let window = window_query.get_single().unwrap();
+    let window_height = window.height();
+
+    if timer.0.tick(time.delta()).just_finished() {
+        for mut transform in &mut query {
+            transform.translation.y = transform.translation.y + 10.0;
+            if transform.translation.y > window_height {
+                transform.translation.y = 0. + (transform.translation.y - window_height);
+            }
+        }
+    }
+}
+
+//ToDo: get the bounding box or height and then fire the bullet from there
+fn udpate_on_button_click(mut query : Query<&Transform, With<Jet>>,
+    // time : Res<Time>,
+    // mut timer : ResMut<SpacePointTimer>,
+    mut commands : Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    keyboard_input : Res<ButtonInput<KeyCode>>){
+    if keyboard_input.pressed(KeyCode::Space) {
+       for tranform in &mut query {
+            commands.spawn((
+                SpacePoint,
+                Bullet,
+                MaterialMesh2dBundle {
+                    mesh: meshes.add(Circle::default()).into(),
+                    material: materials.add(Color::srgb(255., 0.0, 7.5)),
+                    transform: Transform::from_translation(Vec3::new(
+                        tranform.translation.x,
+                        tranform.translation.y,
+                        0.
+                    )),
+                    ..default()
+                },
+            ));
+        } 
+        println!("space bar was pressed here");
+    }
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(SpacePointTimer(Timer::from_seconds(
+            1.0,
+            TimerMode::Repeating,
+        )))
         .add_systems(Startup, setup_system)
         .add_systems(Update, on_resize_system)
-        .add_systems(Update, update_system)
+        .add_systems(Update, (update_background,udpate_on_button_click))
         .run();
 }
